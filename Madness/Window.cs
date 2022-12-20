bd8a62733c66f598600be2fea6959c0d471bbcc8
Rostislav Zhurbenko <91567031+Gw1xo@users.noreply.github.com>
2022-12-20 18:25:54 +0200

    створенно анімацію обертання, підготовано грунт для камери

--- Madness/Window.cs
+++ Madness/Window.cs
@@ -6,17 +6,16 @@ using OpenTK.Windowing.GraphicsLibraryFramework;
 using System.Diagnostics;
 
 
+// змусимо квадрат обертатись
 public class Window : GameWindow
 {
-    // Оскільки ми додаємо текстуру, ми змінюємо масив вершин, щоб включити координати текстури.
-    // Координати текстури в діапазоні від 0,0 до 1,0, де (0,0, 0,0) представляють нижній лівий кут, а (1,0, 1,0) представляють верхній правий.
     private readonly float[] _vertices =
     {
-            // Координати               Координати текстури
-             0.5f,  0.5f, 0.0f,         1.0f, 1.0f, 
-             0.5f, -0.5f, 0.0f,         1.0f, 0.0f, 
-            -0.5f, -0.5f, 0.0f,         0.0f, 0.0f, 
-            -0.5f,  0.5f, 0.0f,         0.0f, 1.0f  
+            // позиція              теккстурні координати
+             0.5f,  0.5f, 0.0f,      1.0f, 1.0f, 
+             0.5f, -0.5f, 0.0f,      1.0f, 0.0f, 
+            -0.5f, -0.5f, 0.0f,      0.0f, 0.0f, 
+            -0.5f,  0.5f, 0.0f,      0.0f, 1.0f  
         };
 
     private readonly uint[] _indices =
@@ -34,7 +33,16 @@ public class Window : GameWindow
     private Shader _shader;
 
     private Texture _texture;
+
     private Texture _texture2;
+
+    // створимо змінну часу, щоб указати, скільки часу минуло з моменту відкриття програми.
+    private double _time;
+
+    // мотім створимо дві матриці для нашого перегляду та проекції
+    private Matrix4 _view;
+    // це показує, як будуть спроектовані вершини.
+    private Matrix4 _projection;
 
     public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
         : base(gameWindowSettings, nativeWindowSettings)
@@ -47,6 +55,9 @@ public class Window : GameWindow
 
         GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
+        // Тут ми вмикаємо тестування глибини.Ми також очищаємо буфер глибини в GL.Clear в OnRenderFrame.
+        GL.Enable(EnableCap.DepthTest);
+
         _vertexArrayObject = GL.GenVertexArray();
         GL.BindVertexArray(_vertexArrayObject);
 
@@ -58,7 +69,7 @@ public class Window : GameWindow
         GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
         GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices, BufferUsageHint.StaticDraw);
 
-        //у вертексний шейдер додано пераметер трансформації матриці
+        // додано юніфікованні в mat4 змінні model, view , projection 
         _shader = new Shader("Shaders/shader.vert", "Shaders/fragshader.frag");
         _shader.Use();
 
@@ -71,43 +82,48 @@ public class Window : GameWindow
         GL.VertexAttribPointer(texCoordLocation, 2, VertexAttribPointerType.Float, false, 5 * sizeof(float), 3 * sizeof(float));
 
         _texture = Texture.LoadFromFile("Resources/container.png");
-
         _texture.Use(TextureUnit.Texture0);
-
 
         _texture2 = Texture.LoadFromFile("Resources/awesomeface.png");
         _texture2.Use(TextureUnit.Texture1);
 
         _shader.SetInt("texture0", 0);
         _shader.SetInt("texture1", 1);
+
+        //  перемістимо вид на три одиниці назад по осі Z.
+        _view = Matrix4.CreateTranslation(0.0f, 0.0f, -3.0f);
+
+        // Для матриці ми використовуємо кілька параметрів.
+        //   Поле зору.
+        //   Співвідношення сторін.
+        //   відсікання ближніх вершин.
+        //   відсікання дальніх вершин.
+        _projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(45f), Size.X / (float)Size.Y, 0.1f, 100.0f);
     }
 
     protected override void OnRenderFrame(FrameEventArgs e)
     {
         base.OnRenderFrame(e);
 
-        GL.Clear(ClearBufferMask.ColorBufferBit);
+        // додаємо час, що минув з останнього кадру, помножений на 4,0 для прискорення анімації, до загальної кількості часу.
+        _time += 4.0 * e.Time;
+
+        // очищаємо буфер глибини
+        GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
         GL.BindVertexArray(_vertexArrayObject);
-
-        // створимо одиничну матрицю
-        var transform = Matrix4.Identity;
-
-        // Тут ми поєднуємо матрицю перетворення з іншою, створеною OpenTK, щоб повернути її на 20 градусів.
-        transform = transform * Matrix4.CreateRotationZ(MathHelper.DegreesToRadians(20f));
-
-        // Далі ми масштабуємо матрицю.
-        transform = transform * Matrix4.CreateScale(1.1f);
-
-        // Потім ми транслюємо матрицю, що трохи переміщує її у верхній правий кут.
-        transform = transform * Matrix4.CreateTranslation(0.1f, 0.1f, 0.0f);
 
         _texture.Use(TextureUnit.Texture0);
         _texture2.Use(TextureUnit.Texture1);
         _shader.Use();
 
-        // уніфікуємо матрицю в шейдер
-        _shader.SetMatrix4("transform", transform);
+        // Нарешті маємо матрицю моделі
+        var model = Matrix4.Identity * Matrix4.CreateRotationX((float)MathHelper.DegreesToRadians(_time));
+
+        // Потім ми передаємо всі ці матриці до вершинного шейдера.
+        _shader.SetMatrix4("model", model);
+        _shader.SetMatrix4("view", _view);
+        _shader.SetMatrix4("projection", _projection);
 
         GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0);
 
